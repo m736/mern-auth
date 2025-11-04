0fc44608e608fda9312f5646fdd351eddfacaf19
m736 <megaladevagi@gmail.com>
2025-11-04 21:11:19 +0530

    without cors

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ const port = process.env.PORT || 4000;
 connectDatabase();
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://mern-auth-oepn.onrender.com",
-];
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://mern-auth-oepn.onrender.com",
+// ];
+// app.use(cors({ origin: allowedOrigins, credentials: true }));
 //app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.get("/", (req, res) => res.send("API working"));
 app.use("/api/auth", authRouter);
