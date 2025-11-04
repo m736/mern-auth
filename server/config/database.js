caa0d030089e675633c7f706397b665f9b7d2587
m736 <megaladevagi@gmail.com>
2025-11-04 22:22:24 +0530

    mongose

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   mongoose.connection.on("connected", () => console.log("Database connected"));
-  await mongoose.connect(`${process.env.DB_LOCAL_URI}`);
+  await mongoose.connect(`${process.env.DB_LOCAL_URI}`, {
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true",
+  });
 };
 export default connectDatabase;
