005e0083be5f69f041bfeb5b1a332d07815a495e
m736 <megaladevagi@gmail.com>
2025-11-04 21:12:16 +0530

    2nd origin

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 //   "http://localhost:5173",
 //   "https://mern-auth-oepn.onrender.com",
 // ];
-// app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 //app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.get("/", (req, res) => res.send("API working"));
 app.use("/api/auth", authRouter);
