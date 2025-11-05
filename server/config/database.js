83ac558afef0e713109b6f05a1966f5b5bc6c5ab
m736 <megaladevagi@gmail.com>
2025-11-05 15:01:04 +0530

    database

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
-
+const connection = {};
 const connectDatabase = async () => {
   try {
-    mongoose.connection.on("connected", () =>
-      console.log("Database connected")
-    );
-    await mongoose.connect(`${process.env.DB_LOCAL_URI}`);
-  } catch {
-    (err) => console.log("Database connection error: ", err);
+    if (connection.isConnected) return;
+    const db = await mongoose.connect(process.env.MONGO);
+    connection.isConnected = db.connections[0].readyState;
+    // mongoose.connection.on("connected", () =>
+    //   console.log("Database connected")
+    // );
+    // await mongoose.connect(`${process.env.DB_LOCAL_URI}`);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
   }
 };
 export default connectDatabase;
