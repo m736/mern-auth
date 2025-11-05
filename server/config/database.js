0685d96d9dbabb7803a1763c75dbdef71843bce9
m736 <megaladevagi@gmail.com>
2025-11-05 14:47:16 +0530

    conne-error

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDatabase = async () => {
-  mongoose.connection.on("connected", () => console.log("Database connected"));
-  await mongoose.connect(`${process.env.DB_LOCAL_URI}`);
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database connected")
+    );
+    await mongoose.connect(`${process.env.DB_LOCAL_URI}`);
+  } catch {
+    (err) => console.log("Database connection error: ", err);
+  }
 };
 export default connectDatabase;
