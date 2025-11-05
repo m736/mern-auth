1644e593d8fe2625882afabc9a1a7f530046a60d
m736 <megaladevagi@gmail.com>
2025-11-05 15:41:13 +0530

    cors issue

--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,7 @@
 import mongoose from "mongoose";
-const connection = {};
+
 const connectDatabase = async () => {
-  try {
-    // if (connection.isConnected) return;
-    // const db = await mongoose.connect(process.env.DB_LOCAL_URI);
-    // connection.isConnected = db.connections[0].readyState;
-    mongoose.connection.on("connected", () =>
-      console.log("Database connected")
-    );
-    await mongoose.connect(`${process.env.DB_LOCAL_URI}`);
-  } catch (error) {
-    console.log(error);
-    throw new Error(error);
-  }
+  mongoose.connection.on("connected", () => console.log("Database connected"));
+  await mongoose.connect(`${process.env.DB_LOCAL_URI}`);
 };
 export default connectDatabase;
